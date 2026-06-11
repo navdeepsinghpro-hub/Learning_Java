aa904c69b604a12f1afd5f82e0702eab9a55ed3e
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-11 12:54:16 +0530

    for loop

--- DAY_02/foe1.java
+++ DAY_02/foe1.java
@@ -2,8 +2,8 @@ package DAY_02;
 
 public class foe1 {
      public static void main(String[] args){
-    for(int i = 0; i<100 ; i += 1){
-        System.out.println("Hello");
+    for(int i = 0; i<10 ; i++){
+        System.out.println(i + " " +"Hello");
     }
     }
 }
