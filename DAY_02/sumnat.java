6030764fe41d0fdb16d241cf2bc0cb2d6cf95e85
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-11 16:59:19 +0530

    sum natural

--- DAY_02/sumnat.java
+++ DAY_02/sumnat.java
@@ -1,5 +1,14 @@
 package DAY_02;
-
+import java.util.*;
 public class sumnat {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i<=n; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
 }
