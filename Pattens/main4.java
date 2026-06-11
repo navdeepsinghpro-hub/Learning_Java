763af078c451e64ca61cca6c75cc3cb9b595c5de
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-06-11 17:55:49 +0530

    Pattrens

---
+++ Pattens/main4.java
@@ -0,0 +1,13 @@
+package Pattens;
+
+public class main4 {
+    public static void main(String[] args){
+        int n = 4;
+        for(int i=1;i<=n;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
