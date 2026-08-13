00d337b27dcaf993b0d1c37e7f6c22b677a57566
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-13 11:57:48 +0530

    question solving

--- DAY_4/Question3.java
+++ DAY_4/Question3.java
@@ -1,5 +1,18 @@
 package DAY_4;
-
+import java.util.Scanner;
 public class Question3 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter principle amount: ");
+        float P = sc.nextFloat();
+        System.out.print("Enter annual intrest rate: ");
+        float R = sc.nextFloat();
+        System.out.print("Enter time in (Years)");
+        float T = sc.nextFloat();
+
+        float SI = (P*R*T)/100;
+        System.out.println("Total simple intrest: " +SI);
+
+        sc.close();
+    }
 }
